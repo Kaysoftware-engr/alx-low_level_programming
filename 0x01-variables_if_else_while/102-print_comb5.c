0aca44abb522baf3060ed32ea72a6ce9a7cb08af
Kaysoftware-engr <jesujanak@gmail.com>
2022-03-11 22:16:13 +0100

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,52 +1,33 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
-/* betty style doc for function main goes there */
 /**
- * main - Entry point
+ *main - entry point
  *
- * Return: Always 0 (Success)
+ *Return:0 - success
+ *
  */
 int main(void)
 {
-	int i, j, k, l;
+	int a, b;
 
-	i = j = k = 48; l = 49;
-	while  ((i < 58))
+	for (a = 0; a < 100; a++)
 	{
-		putchar(i); putchar(j); putchar(32); putchar(k); putchar(l);
-		if ((i == 57) && (j == 56) && (k == 57) && (l == 57))
+		for (b = 1; b < 100; b++)
 		{
-			putchar('\n'); i++;
-		}
-		else
-		{
-			putchar(44); putchar(32);
-			if ((k == 57) && (l == 57))
+			if (a < b)
 			{
-				if (j < 56)
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+				putchar(' ');
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
+				if (!(a == 98 && b == 99))
 				{
-					l = ++j + 1; k = i;
+					putchar(',');
+					putchar(' ');
 				}
-				else if (j == 56)
-				{
-					j++; k = i + 1; l = 48;
-				}
-				else if (j == 57)
-				{
-					j = 48; l = 49; k = ++i;
-				}
-			}
-			else if (l < 57)
-			{
-				l++;
-			}
-			else
-			{
-				l = 48; k++;
 			}
 		}
 	}
+	putchar('\n');
 	return (0);
 }
